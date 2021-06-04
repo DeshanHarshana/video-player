47d8e4479c5ae6e59c1c3c226ceb3eba1b261647
DeshanHarshana <nawarathnadeshan@gmail.com>
2021-06-05 01:18:02 +0530

    Mongodb connect with Mongoose

--- ngApp/server/routes/api.js
+++ ngApp/server/routes/api.js
@@ -1,5 +1,19 @@
 const express=require('express');
+const mongoose  = require('mongoose');
 const router=express.Router();
+
+const db="mongodb+srv://deshan:deshan2233@cluster0.z3fbw.mongodb.net/videoplayer?retryWrites=true&w=majority"
+mongoose.Promise=global.Promise;
+
+mongoose.connect(db,{useNewUrlParser: true,  useUnifiedTopology: true}, function(err){
+  if(err){
+    console.error("Error!" + err);
+  }else{
+    console.log("success");
+  }
+});
+
+
 
 router.get('/', function(req, res){
   res.send('api works');
