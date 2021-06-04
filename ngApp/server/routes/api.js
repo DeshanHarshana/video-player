1ff206e7d02e8dd36948ae80d3bfa93c28870ce1
DeshanHarshana <nawarathnadeshan@gmail.com>
2021-06-05 01:48:34 +0530

    Add get and post data methods

--- ngApp/server/routes/api.js
+++ ngApp/server/routes/api.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const mongoose  = require('mongoose');
 const router=express.Router();
+const Video= require('../models/video');
 
 const db="mongodb+srv://deshan:deshan2233@cluster0.z3fbw.mongodb.net/videoplayer?retryWrites=true&w=majority"
 mongoose.Promise=global.Promise;
@@ -15,8 +16,46 @@ mongoose.connect(db,{useNewUrlParser: true,  useUnifiedTopology: true}, function
 
 
 
-router.get('/', function(req, res){
-  res.send('api works');
+router.get('/videos', function(req, res){
+  console.log("get request for all videos");
+  Video.find({})
+  .exec(function(err,videos){
+    if(err){
+      console.log("Error retriving vidoes");
+    }else{
+      res.json(videos);
+    }
+  });
+});
+
+
+router.get('/videos/:id', function(req, res){
+  console.log("get request for a single video");
+  Video.findById(req.params.id)
+  .exec(function(err,videos){
+    if(err){
+      console.log("Error retriving vidoes");
+    }else{
+      res.json(videos);
+    }
+  });
+});
+
+
+router.post('/video', function(req,res){
+  console.log("Post a video");
+  var newVideo=new Video();
+  newVideo.title=req.body.title;
+  newVideo.url=req.body.url;
+  newVideo.description=req.body.description;
+  newVideo.save(function(err, insertedVideo){
+    if(err){
+      console.log("Error saving video");
+    }
+    else{
+      res.json(insertedVideo);
+    }
+  });
 });
 
 module.exports=router;
